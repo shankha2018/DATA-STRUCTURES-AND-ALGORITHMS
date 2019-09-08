8177dc3e1330c3f24a7ca20a23492dc73283a465
Disha Sinha <42354197+disha2sinha@users.noreply.github.com>
2019-09-08 20:39:27 +0530

    Add files via upload

---
+++ Arrays/Array_Rotation(using Temporary array).c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+	int arr[20],key,i;
+	printf("Enter Elements Of the Array:\n");
+	for(i=0;i<n;i++)
+	{
+		printf("\nEnter %dth Element:");
+		scanf("%d",&arr[i]);
+	}
+	printf("Enter")
+}
